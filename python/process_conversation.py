4881f07db12aeec5763add44e8c1ae5ab0fd3bd9
unknown <dktulsi@mail.usf.edu>
2021-09-25 12:47:31 -0400

    added test file for using symbl.ai

---
+++ python/process_conversation.py
@@ -0,0 +1,15 @@
+"""
+Transcript
+"""
+import json
+import requests
+import googletrans
+
+# # set your access token here. See https://docs.symbl.ai/docs/authentication
+access_token = 'AUTH_TOKEN'
+
+# # link to audio,text or video
+
+# #
+# get transcript to first responder
+# action - communicate with other responders
